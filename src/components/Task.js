d66ec5c4d33bec4cec3ce057f91318a73eb2ad49
Ciphr7 <gil@ciphr7.com>
2022-04-18 03:17:06 -0500

    half way

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,11 @@
+import {FaTimes} from 'react-icons/fa'
 
-
-const Task = () => {
+const Task = ({task, onDelete, onToggle}) => {
   return (
-    <div className="task">My Task</div>
+    <div className={`task ${task.reminder ? 'reminder' : ''}`} onClick= { () => onToggle(task.id)}>
+      <h3>{task.text} <FaTimes style={{color: 'red', cursor: 'pointer' }} onClick={()=> onDelete(task.id) } /></h3>
+      <p>{task.day}</p>
+    </div>
   )
 }
 
